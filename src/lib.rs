cbfc7517295507b6009fb214f04e1569a3809db9
Malik Olivier Boussejra <malik@boussejra.com>
2018-04-10 17:53:03 +0900

    lib.rs: Include quick how to use

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+//! Library to parse FITS file written in pure rust.
+//!
+//! # How to use
+//!
+//! ```rust,no_run
+//! extern crate fitrs;
+//! use fitrs::{Fits, FitsData, FitsDataArray};
+//!
+//! let fits = Fits::open("path/to/fits/file.fits").expect("Failed to open");
+//! // Iterate over HDUs
+//! for hdu in fits.iter() {
+//!     println!("{:?}", hdu.value("EXTNAME"));
+//!     println!("{:?}", hdu.read_data());
+//! }
+//!
+//! // Get HDU by ID
+//! let hdu_2 = &fits[2];
+//! // Get HDU by EXTNAME
+//! let hdu_flux = &fits["FLUX"];
+//!
+//! match hdu_flux.read_data() {
+//!     &FitsData::FloatingPoint32(FitsDataArray { ref shape, ref data }) => {
+//!         println!("{:?}", shape);
+//!         println!("{:?}", data);
+//!     }
+//!     _ => { /* ... */ }
+//! }
+//! ```
+
 extern crate byteorder;
 
 mod fits;
