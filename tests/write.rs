b553bfd916383b82c161ebca6de90653d8afee8e
Malik Olivier Boussejra <malik@boussejra.com>
2018-09-20 16:02:09 +0900

    Do not use flush in write test

--- tests/write.rs
+++ tests/write.rs
@@ -5,6 +5,5 @@ use fitrs::{Fits, Hdu};
 #[test]
 fn write_single_hdu_file() {
     let primary_hdu = Hdu::new(&[10, 10], vec![0.0f32; 100]);
-    let mut fits = Fits::empty("out.fits", primary_hdu);
-    fits.flush().expect("Saved!");
+    let mut _fits = Fits::create("out.fits", primary_hdu).expect("created!");
 }
