9523f4f3298868a13adcff8e7dcbecd750a9d9e6
Malik Olivier Boussejra <malik@boussejra.com>
2018-09-21 09:07:40 +0900

    [verify] Allow to check arbitrary FITS file

--- verify/verify.py
+++ verify/verify.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
+import argparse
 from astropy.io import fits
 
-fits_filepath = "../test/testprog.fit"
+parser = argparse.ArgumentParser(description="FITS file verifier")
+parser.add_argument('file', type=argparse.FileType('r'),
+    help='FITS file to check')
+
+args = parser.parse_args()
+
+fits_filepath = args.file.name
+args.file.close()
 
 hdus = fits.open(fits_filepath)
 
