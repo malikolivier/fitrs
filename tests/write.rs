0cd1c4a2f28eaaebf4ed83bb4c891debe968a8a7
Malik Olivier Boussejra <malik@boussejra.com>
2018-09-22 12:41:29 +0900

    [tests] write: Add test for multiple-Hdu files

--- tests/write.rs
+++ tests/write.rs
@@ -22,3 +22,25 @@ fn write_single_hdu_file() {
     assert_eq!(output.stderr, vec![], "No error output");
     assert!(output.status.success(), "Exit with success");
 }
+
+#[test]
+fn write_multiple_hdu_file() {
+    let data: Vec<i32> = (0..20)
+        .map(|i| (0..20).map(move |j| i + j))
+        .flatten()
+        .collect();
+    let primary_hdu = Hdu::new(&[20, 20], data.clone());
+    let mut fits = Fits::create("out.fits", primary_hdu).expect("created!");
+
+    let secondary_hdu = Hdu::new(&[20, 20], data);
+    fits.push(secondary_hdu).expect("Pushed");
+
+    let output = Command::new("verify/verify.py")
+        .arg("out.fits")
+        .output()
+        .expect("Failed to execute verify command");
+
+    assert_eq!(output.stdout, vec![], "No standard output");
+    assert_eq!(output.stderr, vec![], "No error output");
+    assert!(output.status.success(), "Exit with success");
+}
