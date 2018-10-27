b343c2bf63b76d656efea1ded89710d1ea1ac20b
Malik Olivier Boussejra <malik@boussejra.com>
2018-10-27 16:28:41 +0900

    [wcs] Do not use transformation matrix
    It is simpler this way. Assume that the transformation matrix is
    diagonal.

--- src/wcs.rs
+++ src/wcs.rs
@@ -12,25 +12,11 @@ pub struct WCS {
 struct CoordCoefs {
     crpix: f32,
     crval: f32,
-    cds: [f32; MAX_DIM],
+    cd: f32,
 }
 
 impl WCS {
     pub fn new(hdu: &Hdu) -> Self {
-        fn get_cds(hdu: &Hdu, n: usize) -> [f32; MAX_DIM] {
-            let mut cds = [0.0; MAX_DIM];
-            for (m, cd) in cds.iter_mut().enumerate() {
-                let cd_key = format!("CD{}_{}", n + 1, m + 1);
-                *cd = if let Some(HeaderValue::RealFloatingNumber(cd)) = hdu.value(&cd_key) {
-                    *cd as f32
-                } else if n == m {
-                    1.0
-                } else {
-                    0.0
-                }
-            }
-            cds
-        }
         fn get_number(hdu: &Hdu, key: &str) -> Option<f32> {
             match hdu.value(key) {
                 Some(HeaderValue::RealFloatingNumber(f)) => Some(*f as f32),
@@ -43,22 +29,25 @@ impl WCS {
         let mut coefs = [CoordCoefs {
             crpix: 0.0,
             crval: 0.0,
-            cds: [0.0; MAX_DIM],
+            cd: 0.0,
         }; MAX_DIM];
         for (n, coef) in coefs.iter_mut().enumerate() {
             let crpix_key = format!("CRPIX{}", n + 1);
             let crval_key = format!("CRVAL{}", n + 1);
-            if let (Some(crpix), Some(crval)) =
-                (get_number(hdu, &crpix_key), get_number(hdu, &crval_key))
-            {
+            let cd_key = format!("CD{}_{}", n + 1, n + 1);
+            if let (Some(crpix), Some(crval), Some(cd)) = (
+                get_number(hdu, &crpix_key),
+                get_number(hdu, &crval_key),
+                get_number(hdu, &cd_key),
+            ) {
                 // FITS are 1-indexed, so correct offset so that it becomes 0-indexed.
                 coef.crpix = crpix - 1.0;
                 coef.crval = crval;
+                coef.cd = cd;
                 size += 1;
             } else {
                 break;
             }
-            coef.cds = get_cds(hdu, n);
         }
         Self { coefs, size }
     }
@@ -70,7 +59,7 @@ impl WCS {
     pub fn pix2world(&self, mut pixel: [f32; MAX_DIM]) -> [f32; MAX_DIM] {
         for (i, p) in pixel.iter_mut().enumerate().take(self.size) {
             let coef = &self.coefs[i];
-            *p = coef.crval + (*p - coef.crpix) * coef.cds[i];
+            *p = coef.crval + (*p - coef.crpix) * coef.cd;
         }
         pixel
     }
@@ -80,7 +69,6 @@ impl WCS {
         out.size = indices.len();
         for (i, idx) in indices.iter().enumerate() {
             out.coefs[i] = self.coefs[*idx];
-            out.coefs[i].cds[i] = self.coefs[*idx].cds[*idx];
         }
         out
     }
