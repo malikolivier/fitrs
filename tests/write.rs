31865a73132db9db4651c089e803e4d2532597af
Malik Olivier Boussejra <malik@boussejra.com>
2018-11-16 17:03:40 -0500

    [tests] write: Add integration test for writing continued string
    Interestingly, the test is failing for the reasons explained in the
    code. I do not understand one and think will drop an email to the FITS
    or astropy mailing list.

--- tests/write.rs
+++ tests/write.rs
@@ -2,7 +2,7 @@ extern crate fitrs;
 
 use std::process::Command;
 
-use fitrs::{Fits, Hdu};
+use fitrs::{Fits, Hdu, HeaderValue};
 
 #[test]
 fn write_single_hdu_file() {
@@ -34,6 +34,28 @@ fn write_multiple_hdu_file() {
 
     let secondary_hdu = Hdu::new(&[20, 20], data);
     fits.push(secondary_hdu).expect("Pushed");
+
+    let output = Command::new("verify/verify.py")
+        .arg("out.fits")
+        .output()
+        .expect("Failed to execute verify command");
+
+    assert_eq!(output.stdout, vec![], "No standard output");
+    assert_eq!(output.stderr, vec![], "No error output");
+    assert!(output.status.success(), "Exit with success");
+}
+
+#[test]
+fn write_file_with_long_string() {
+    let mut primary_hdu = Hdu::new(&[1], vec![0]);
+    primary_hdu.insert(
+        "TEST",
+        HeaderValue::CharacterString(
+            "This is a very long string value that is continued over more than one keyword."
+                .to_owned(),
+        ),
+    );
+    let _ = Fits::create("out.fits", primary_hdu).expect("created!");
 
     let output = Command::new("verify/verify.py")
         .arg("out.fits")
